655daecc3bdd8a0574e33a6494cc56ce248292d2
iremkaraoglu <iremk97@gmail.com>
2021-10-15 15:26:55 +0300

    Initial Commit

---
+++ Accessibility Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accessibility Example
+//
+//  Created by Irem Karaoglu on 15.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
