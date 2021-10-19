b782fc58dfc1cd86ee6b50215ebfe334577467bc
iremkaraoglu <iremk97@gmail.com>
2021-10-19 22:09:21 +0300

    improve initial panda screen

--- Accessibility Example/ViewController.swift
+++ Accessibility Example/ViewController.swift
@@ -11,14 +11,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         
-        let textLabel = UILabel()
-        textLabel.text = "Hey Listen!"
-        textLabel.textColor = .systemPink
-        textLabel.frame = .init(x: 40, y: 40, width: 100, height: 100)
+//        view.backgroundColor = UIColor(red: 152, green: 251, blue: 152, alpha: 1)
         
-        view.addSubview(textLabel)
+        let pandaLabel = UILabel()
+        pandaLabel.text = "Panda Bear"
+        pandaLabel.textColor = .darkGray
+        pandaLabel.font = .boldSystemFont(ofSize: 32)
+        pandaLabel.frame = .init(x: 40, y: 40, width: 100, height: 100)
+        pandaLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pandaLabel)
+        
+        let pandaImageView = UIImageView()
+        pandaImageView.image = UIImage(named: "panda")
+        pandaImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pandaImageView)
+        
+        let nextButton = UIButton()
+        nextButton.setTitle("Next", for: .normal)
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.backgroundColor = .systemIndigo
+        nextButton.layer.cornerRadius = 28
+        view.addSubview(nextButton)
+        
+        NSLayoutConstraint.activate([
+            pandaLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pandaLabel.centerYAnchor.constraint(equalTo: pandaImageView.topAnchor, constant: -80),
+            pandaImageView.widthAnchor.constraint(equalToConstant: 300),
+            pandaImageView.heightAnchor.constraint(equalToConstant: 400),
+            pandaImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pandaImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 100),
+            nextButton.heightAnchor.constraint(equalToConstant: 60),
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: pandaImageView.bottomAnchor, constant: 60),
+            ])
         
         
         
