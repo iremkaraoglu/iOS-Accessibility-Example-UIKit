0516ff5cf73ff52e85363239f8b102eb8d0f5a3a
iremkaraoglu <iremk97@gmail.com>
2021-11-15 16:57:41 +0300

    add question view controller

---
+++ Accessibility Example/QuestionViewController.swift
@@ -0,0 +1,83 @@
+//
+//  QuestionViewController.swift
+//  Accessibility Example
+//
+//  Created by Irem Karaoglu on 12.11.2021.
+//
+
+import Foundation
+import UIKit
+
+class QuestionViewController: UIViewController {
+    let step:Float = 1 // If you want UISlider to snap to steps by 10
+    let pandaLabel = UILabel()
+    let loveCount = UILabel()
+    let pandaImageView = UIImageView()
+    
+    override func viewDidLoad() {
+        
+        view.backgroundColor = UIColor(red: 173/255, green: 217/255, blue: 185/255, alpha: 1.0)
+        
+        pandaLabel.text = "How much do you love pandas?"
+        pandaLabel.textColor = .black
+        pandaLabel.font = .boldSystemFont(ofSize: 24)
+        pandaLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pandaLabel)
+        
+        pandaImageView.image = UIImage(named: "panda2")
+        pandaImageView.layer.cornerRadius = 20
+        pandaImageView.layer.borderWidth = 1
+        pandaImageView.layer.masksToBounds = false
+        pandaImageView.layer.cornerRadius = 20
+        pandaImageView.clipsToBounds = true
+        pandaImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pandaImageView)
+        
+        let loveSlider = UISlider(frame:CGRect(x: 0, y: 0, width: 300, height: 20))
+        loveSlider.minimumValue = 0
+        loveSlider.maximumValue = 100
+        loveSlider.value = 80
+        loveSlider.isContinuous = true
+        loveSlider.tintColor = UIColor.darkGray
+        loveSlider.thumbTintColor = UIColor.systemPink
+        loveSlider.translatesAutoresizingMaskIntoConstraints = false
+        loveSlider.addTarget(self, action: #selector(self.sliderValueDidChange(_:)), for: .valueChanged)
+        view.addSubview(loveSlider)
+        
+        UIView.animate(withDuration: 0.8) {
+            loveSlider.setValue(80.0, animated: true)
+        }
+        
+        
+        loveCount.textColor = .black
+        loveCount.text = String(Int(loveSlider.value))
+        loveCount.font = .boldSystemFont(ofSize: 28)
+        loveCount.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loveCount)
+        
+        NSLayoutConstraint.activate([
+            pandaLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pandaLabel.centerYAnchor.constraint(equalTo: pandaImageView.topAnchor, constant: -80),
+            pandaImageView.widthAnchor.constraint(equalToConstant: 300),
+            pandaImageView.heightAnchor.constraint(equalToConstant: 400),
+            pandaImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pandaImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loveCount.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loveCount.centerYAnchor.constraint(equalTo: loveSlider.topAnchor, constant: -10),
+            loveSlider.widthAnchor.constraint(equalToConstant: 300),
+            loveSlider.heightAnchor.constraint(equalToConstant: 80),
+            loveSlider.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loveSlider.centerYAnchor.constraint(equalTo: pandaImageView.bottomAnchor, constant: 90),
+        ])
+    }
+    
+    @objc func sliderValueDidChange(_ sender:UISlider!)
+    {
+        let roundedStepValue = round(sender.value / step) * step
+        sender.value = roundedStepValue
+        print("Slider step value \(Int(roundedStepValue))")
+        loveCount.text = String(Int(roundedStepValue))
+    }
+}
+
+
--- Accessibility Example/ViewController.swift
+++ Accessibility Example/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor(red: 173/255, green: 217/255, blue: 185/255, alpha: 1.0)
         
         let pandaLabel = UILabel()
-        pandaLabel.text = "Get to know pandas 🐼"
+        pandaLabel.text = "Welcome"
         pandaLabel.textColor = .black
         pandaLabel.font = .boldSystemFont(ofSize: 32)
         pandaLabel.frame = .init(x: 40, y: 40, width: 100, height: 100)
@@ -24,61 +24,6 @@ class ViewController: UIViewController {
         
         let pandaImageView = UIImageView()
         pandaImageView.image = UIImage(named: "panda")
-        pandaImageView.layer.cornerRadius = 20
-        pandaImageView.layer.borderWidth = 1
-        pandaImageView.layer.masksToBounds = false
-        pandaImageView.layer.cornerRadius = 20
-        pandaImageView.clipsToBounds = true
-        pandaImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(pandaImageView)
-        
-        let nextButton = UIButton()
-        nextButton.setTitle("Start learning", for: .normal)
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.backgroundColor = .black
-        nextButton.layer.cornerRadius = 28
-        nextButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-        view.addSubview(nextButton)
-        
-        NSLayoutConstraint.activate([
-            pandaLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            pandaLabel.centerYAnchor.constraint(equalTo: pandaImageView.topAnchor, constant: -80),
-            pandaImageView.widthAnchor.constraint(equalToConstant: 300),
-            pandaImageView.heightAnchor.constraint(equalToConstant: 400),
-            pandaImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            pandaImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            nextButton.widthAnchor.constraint(equalToConstant: 180),
-            nextButton.heightAnchor.constraint(equalToConstant: 60),
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nextButton.centerYAnchor.constraint(equalTo: pandaImageView.bottomAnchor, constant: 90),
-            ])
-    }
-    
-    @objc private func didTapButton() {
-        let secondVC = SecondViewController()
-//        secondVC.title = "Welcome"
-        navigationController?.pushViewController(secondVC, animated: true)
-    }
-}
-
-class SecondViewController: UIViewController {
-    
-    let nextButton = UIButton()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 173/255, green: 217/255, blue: 185/255, alpha: 1.0)
-        
-        let pandaLabel = UILabel()
-        pandaLabel.text = "Who are pandas?"
-        pandaLabel.textColor = .black
-        pandaLabel.font = .boldSystemFont(ofSize: 32)
-        pandaLabel.frame = .init(x: 40, y: 40, width: 100, height: 100)
-        pandaLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(pandaLabel)
-        
-        let pandaImageView = UIImageView()
-        pandaImageView.image = UIImage(named: "panda2")
         pandaImageView.layer.cornerRadius = 20
         pandaImageView.layer.borderWidth = 1
         pandaImageView.layer.masksToBounds = false
@@ -107,37 +52,12 @@ class SecondViewController: UIViewController {
             nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             nextButton.centerYAnchor.constraint(equalTo: pandaImageView.bottomAnchor, constant: 90),
             ])
-        
     }
     
     @objc private func didTapButton() {
-        let vc = ThirdViewController()
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    class ThirdViewController: UIViewController {
-        
-        let nextButton = UIButton()
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = UIColor(red: 173/255, green: 217/255, blue: 185/255, alpha: 1.0)
-            
-            let pandaLabel = UILabel()
-            pandaLabel.text = "The giant panda (Ailuropoda melanoleuca; Chinese: 大熊猫; pinyin: dàxióngmāo),[5] also known as the panda bear (or simply the panda), is a bear[6] native to South Central China.[1] It is characterised by its bold black-and-white coat and rotund body."
-            pandaLabel.textColor = .black
-            pandaLabel.font = .boldSystemFont(ofSize: 32)
-            pandaLabel.frame = .init(x: 40, y: 40, width: 100, height: 100)
-            pandaLabel.numberOfLines = 0
-            pandaLabel.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(pandaLabel)
-            
-            
-            
-            NSLayoutConstraint.activate([
-                pandaLabel.heightAnchor.constraint(equalTo: view.heightAnchor),
-                pandaLabel.widthAnchor.constraint(equalTo: view.widthAnchor),
-                ])            
-        }
+        let secondVC = QuestionViewController()
+//        secondVC.title = "Welcome"
+        navigationController?.pushViewController(secondVC, animated: true)
     }
 }
+
